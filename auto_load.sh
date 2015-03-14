8aec4b8b15cd10359a5c26c7c9e5687abe7a91fa
Baimurzin Vladislav <baimurzin.719@gmail.com>
2015-03-14 19:07:04 +0400

    bash script added

--- auto_load.sh
+++ auto_load.sh
@@ -6,4 +6,5 @@ rm groups.dat
 mv vk_auto_post/main.php ./
 mv vk_auto_post/groups.dat ./
 
-echo -en "\033[37;1;41m All Done!!! \033[0m \n"
+echo -en "\033[37;1;41m All Done!!! \033[0m"
+echo -n "   "

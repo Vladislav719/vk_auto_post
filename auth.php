8c95b22ef10c965e4b4964d5b1a4cb7f09b4c391
Baimurzin Vladislav <baimurzin.719@gmail.com>
2015-03-14 19:49:34 +0400

    bash script added

--- auth.php
+++ auth.php
@@ -27,7 +27,7 @@ $authorize_url = $vk->getAuthorizeURL(
     $vk_config['api_settings'], $vk_config['callback_url']);
 echo '<a href="' . $authorize_url . '">Sign in with VK</a>';
 echo 1;
-$access_token = $vk->getAccessToken('3d2d41fbcebfe18bc5', $vk_config['callback_url']);
+$access_token = $vk->getAccessToken('e46c3be5ccfeb30c31', $vk_config['callback_url']);
 echo 'access token: ' . $access_token['access_token']
     . '<br />expires: ' . $access_token['expires_in'] . ' sec.'
     . '<br />user id: ' . $access_token['user_id'] . '<br /><br />';
